05b3e28d89e6f1780173862fce017fb057f51d7c
Sergey Ilin <silyinnur@yandex.ru>
2022-12-24 09:46:24 +0500

    add blazorise template

---
+++ NewYear.BlazorServer/Program.cs
@@ -0,0 +1,39 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using NewYear.BlazorServer.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ NewYearBlazorise/NewYearBlazorise/Components/TodoApp/Filter.cs
@@ -0,0 +1,9 @@
+﻿namespace NewYearBlazorise.Components.TodoApp
+{
+    public enum Filter
+    {
+        All,
+        Active,
+        Completed,
+    }
+}
